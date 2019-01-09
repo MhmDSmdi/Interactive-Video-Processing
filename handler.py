dabd743619b3d75777d95170d9b8973ba085b874
mhmdsmdi <mhmd.samadi76@gmail.com>
2019-01-09 20:56:59 +0330

    rotation, guassian blur, gray scale image add

--- handler.py
+++ handler.py
@@ -9,10 +9,12 @@ class ImageHandler:
 
     def __init__(self):
         self.image = cv2.imread(self.IMAGE_ADDRESS)
-        print(self.image.shape)
 
-    def show_image(self, img=image):
-        cv2.imshow('Image', img)
+    def show_image(self, img=image, title="Image"):
+        if img is None:
+            cv2.imshow(title, self.image)
+        else:
+            cv2.imshow(title, img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
@@ -29,12 +31,26 @@ class ImageHandler:
         elif channel is 'G':
             rgb_filter[:, :, 2] = 0
             rgb_filter[:, :, 0] = 0
-        self.show_image(rgb_filter)
+        self.show_image(rgb_filter, channel + str("_Channel"))
 
-    def get_key(prompt):
+    def show_gray_scale_image(self):
+        gray_scale_image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        self.show_image(gray_scale_image, "Gray Scale Image")
+
+    def show_gaussian_blur_image(self):
+        blur = cv2.GaussianBlur(self.image, (25, 25), 0)
+        self.show_image(blur, "Gaussian Smoothing")
+
+    def show_rotation_image(self, angel):
+        rows, cols, channels = self.image.shape
+        M = cv2.getRotationMatrix2D((cols / 2, rows / 2), angel, 1)
+        dst = cv2.warpAffine(self.image, M, (cols, rows))
+        self.show_image(dst, str(angel) + " Rotation")
+
+    def get_key(self, prompt):
         return input(prompt)
 
 
 if __name__ == '__main__':
     imageHandler = ImageHandler()
-    imageHandler.show_color_channel('B')
+
